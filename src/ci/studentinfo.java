d87fcba18094acd0fb073285a4f7d5d393b37fc0
sabar <sabar@LAPTOP-OI80RNO9>
2026-01-11 19:32:23 +0530

    first commit

---
+++ src/ci/studentinfo.java
@@ -0,0 +1,10 @@
+package ci;
+
+public class studentinfo {
+	public static void main(String[] args) {
+        System.out.println("Student Name: SRI VAISHNAVI V");
+        System.out.println("Register Number: 23MIC0102");
+        System.out.println("Jenkins–GitHub Integration Successful");
+	}
+
+}
