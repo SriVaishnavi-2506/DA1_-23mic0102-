c944d773471f3eee86ce7535f3a0bf6be087bfc6
sabar <sabar@LAPTOP-OI80RNO9>
2026-01-11 19:53:07 +0530

    second commit

--- src/ci/studentinfo.java
+++ src/ci/studentinfo.java
@@ -5,6 +5,7 @@ public class studentinfo {
         System.out.println("Student Name: SRI VAISHNAVI V");
         System.out.println("Register Number: 23MIC0102");
         System.out.println("Jenkins–GitHub Integration Successful");
-	}
+        System.out.println("second commit succesfull")	;
+        }
 
 }
